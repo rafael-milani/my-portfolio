21eec7b015fac6d340d46c592463d7b9ecf2d83d
rafael-milani <rafamilanimarangoni@gmail.com>
2024-05-20 17:17:13 -0300

    feat: creates Hero components UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-
+import Home from "./assets/pages/Home/Home";
 
 const App = () => {
-
-
   return (
     <>
-    App
+      <Home />
     </>
-      
-  )
-}
+  );
+};
 
-export default App
+export default App;
